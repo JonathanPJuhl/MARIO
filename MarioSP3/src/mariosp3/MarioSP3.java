28607c8e2216e27ac25d839da05cd66342c5326c
hassanainali <hassanainali@10.50.130.97>
2019-04-08 11:26:20 +0200

    tilføjet klasser

--- MarioSP3/src/mariosp3/MarioSP3.java
+++ MarioSP3/src/mariosp3/MarioSP3.java
@@ -6,6 +6,8 @@ public class MarioSP3 {
     
     public static void main(String[] args) {
         System.out.println("Hej");
+        
+        System.out.println("hejsa god weekend");
     
 
     }
