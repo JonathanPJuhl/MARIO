15076638ffc60d3c43a10fca7b172922015eb516
Ashayla <Ashayla@10.50.137.228>
2019-04-08 11:02:10 +0200

    fuck dig

--- MarioSP3/src/mariosp3/MarioSP3.java
+++ MarioSP3/src/mariosp3/MarioSP3.java
@@ -7,6 +7,7 @@ public class MarioSP3 {
     public static void main(String[] args) {
         System.out.println("Hej");
         System.out.println("Halli halløj");
+        System.out.println("fuck dig");
     
 
     }
