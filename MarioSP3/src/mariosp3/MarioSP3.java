d0367a72f6efef6688f8a6468ca76152f2a935b4
jonat <jonat@10.50.137.51>
2019-04-09 10:39:15 +0200

    Har skrevet "ÆÆÆÆÆÆ"

--- MarioSP3/src/mariosp3/MarioSP3.java
+++ MarioSP3/src/mariosp3/MarioSP3.java
@@ -2,14 +2,14 @@ package mariosp3;
 
 
 public class MarioSP3 {
-
+    
     
     public static void main(String[] args) {
         System.out.println("Hej");
         System.out.println("Halli halløj");
         System.out.println("fuck dig");
     
-
+        System.out.println("ÆÆÆÆÆÆ");
     }
 
 }
